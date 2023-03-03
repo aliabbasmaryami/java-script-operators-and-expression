6ee058a6af99c26dbf172fe46f79875e0fea0e02
Ali abbas <94835254+aliabbasmaryami@users.noreply.github.com>
2023-03-03 19:50:25 +0500

    Add files via upload

---
+++ practice.java/js/script.js
@@ -0,0 +1,53 @@
+// let a = 10;
+// let b = 5;
+
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+
+
+// console.log("++a = ", ++a);
+// console.log("a++ = ", a++);
+// console.log("--a = ", --a);
+// console.log("a-- = ", a--);
+// console.log("a = ", a);
+// console.log("a-- = ", a--);
+
+
+//   assingments operators
+
+// let a = 10;
+// // let b = 5;
+
+// let assinngment = 1;
+// assinngment = a+7;
+// console.log(assinngment);
+// let mult = 1;
+// assinngment = a*7;
+// console.log(assinngment);
+// let sub = 1;
+// assinngment = a-7;
+// console.log(assinngment);
+// let exponents = 1;
+// assinngment = a**7;
+// console.log(assinngment);
+
+// comparison opperators
+
+// let com1 = 6;
+// let com2 = "6";
+// console.log("com1 == comt2", com1 == com2)
+// console.log("com1 != comt2", com1 != com2)
+// console.log("com1 === comt2", com1 === com2)
+// console.log("com1 !== comt2", com1 !== com2)
+
+// logical operator
+
+let a = 5
+let b = 10
+console.log(a<b && a==5)
+console.log(a>b || a==5)
+console.log(!false)
+
